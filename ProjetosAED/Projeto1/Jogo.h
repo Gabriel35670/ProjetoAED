2b805a29617f0b4a41d64f9abd967bc6bfc2e9db
Pedro Gonçalves dos Santos <pedrogsantos_10@hotmail.com>
2021-06-12 13:05:30 -0300

    update jogo

--- ProjetosAED/Projeto1/Jogo.h
+++ ProjetosAED/Projeto1/Jogo.h
@@ -1,18 +1,28 @@
 #include <vector>
 #include <algorithm>
 
-
 #include "Jogador.h"
 #include "Pilha.h"
 #include "No.h"
 #include "Fruta.h"
 
+class Jogo
+{
 
-class Jogo{
+private:
+    vector<Jogador *> lista_jogadores;
+    int tempoSequência;
+    int tempoGameplay;
 
-    private:
+public:
+    Jogo(int tempoSeq, int tempoGam);
+    ~Jogo();
 
-        vector<Jogador*> lista_jogadores;
-        int rodada;
-        
+    void setTempoSeq(int);
+    int getTempoSeq() const;
+    void setTempoGam(int);
+    int getTempoGam() const;
+    void embaralha(PILHA *P);
+    void iniciarTempoGam();
+    void iniciarTempoSeq();
 };
